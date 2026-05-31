17b15be52ed9e53e8c8d23514ce2a36168cb1ecc
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-05-31 11:39:44 -0400

    feat(api): implement user sign-in with PocketBase

--- app/api/v1/signin/route.ts
+++ app/api/v1/signin/route.ts
@@ -1,32 +1,49 @@
+import { pb } from "@/server/pocketbase";
+
 export async function POST(request: Request) {
   try {
-    const authorization = request.headers.get("Authorization");
+    const body = await request.json();
 
-    if (!authorization) {
-      return Response.json(
-        {
-          success: false,
-          message: "Authorization header is required.",
-        },
-        { status: 401 },
-      );
-    }
+    const { email, password } = body;
+
+    const authData = await pb
+      .collection("users")
+      .authWithPassword(email, password);
 
     return Response.json(
       {
         success: true,
-        message: "Signed out successfully.",
+        message: "Signed in successfully.",
+        token: authData.token,
+        data: {
+          user: {
+            id: authData.record.id,
+            firstname: authData.record.firstname,
+            lastname: authData.record.lastname,
+            email: authData.record.email,
+            avatar: authData.record.avatar,
+            verified: authData.record.verified,
+            createdat: authData.record.created,
+            updatedat: authData.record.updated,
+          },
+          token: authData.token
+        },
       },
       { status: 200 },
     );
   } catch (error: any) {
+    console.error(error);
+
     return Response.json(
       {
         success: false,
-        message: "Failed to sign out",
-        error: error?.message || "Unknown error",
+        message: "Failed to sign in",
+        error: error?.response?.message || error?.message || "Unknown error",
+        data: error?.response?.data,
       },
-      { status: 500 },
+      {
+        status: error?.status || 401,
+      },
     );
   }
 }
