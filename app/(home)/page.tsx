56de2375446dafafe53879638f15001420c350af
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-06-06 20:12:34 -0400

    refactor(page): remove deprecated featured product ID and related comments

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -4,11 +4,6 @@ import { Datamock } from "@/mock/mock";
 import ProductService from "@/services/products/ProductService";
 import { ProductsInteractive } from "./ProductsInteractive";
 
-// WARNING: ducktape code
-// this number refers to the id of featured products in dbms so if featured is other number this fails
-const featuredproductcategoryid = 1; // WARNING: this could change in the dbms so this has to change so it autodetects featured tag this is a temporary fix if error check here related to products in home screen 
-
-
 export default async function Page() {
   const productService = new ProductService();
   const products = await productService.getProducts({
