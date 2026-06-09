80a1ee2719b5edde8e19c697e23cd3e0db5eeed8
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-06-09 18:52:07 -0400

    refactor(home/page): replace mock data with dynamic headlines in HeroSection

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,5 @@
 import { HeroSection } from "@/components/childcomponents/home/sections/hero";
 import { TrustBadgeStrip } from "@/components/childcomponents/home/sections/TrustBadgeStrip";
-import { Datamock } from "@/mock/mock";
 import ProductService from "@/services/products/ProductService";
 import { ProductsInteractive } from "./ProductsInteractive";
 
@@ -11,12 +10,12 @@ export default async function Page() {
     limit: 20,
     offset: 0,
   });
+
+  const preheadline = "Universidad Autonoma de Santo Domingo Semestre 2026-01";
+  const headline = "Todo lo que necesitas para tu vida *universitaria*";
   return (
     <main>
-      <HeroSection
-        preheadline={Datamock.homepage.herosection.preheadline}
-        headline={Datamock.homepage.herosection.headline}
-      />
+      <HeroSection preheadline={preheadline} headline={headline} />
 
       <ProductsInteractive products={products} />
 
