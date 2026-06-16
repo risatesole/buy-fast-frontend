7da24d1db8aeef9a4a428381a40979a47acb4c02
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-06-16 19:03:42 -0400

    feat(admin-customer): add table mock of customers

--- app/admin/customers/page.tsx
+++ app/admin/customers/page.tsx
@@ -1,3 +1,69 @@
-export default function CustomerPage() {
-  return <p>Welcome to customer admin page page</p>;
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
+export default function UsersPage() {
+  const users = [
+    {
+      id: 1,
+      firstname: "Henry",
+      lastname: "Ramirez",
+      email: "henry@example.com",
+      role: "Admin",
+      status: true,
+    },
+    {
+      id: 2,
+      firstname: "Ana",
+      lastname: "jhons",
+      email: "ana@example.com",
+      role: "User",
+      status: false,
+    },
+  ];
+
+  return (
+    <div className="container mx-auto py-10">
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Id</TableHead>
+            <TableHead>First Name</TableHead>
+            <TableHead>Last Name</TableHead>
+            <TableHead>Email</TableHead>
+            <TableHead>Role</TableHead>
+            <TableHead>status</TableHead>
+          </TableRow>
+        </TableHeader>
+
+        <TableBody>
+          {users.map((user) => (
+            <TableRow key={user.id}>
+              <TableCell>{user.id}</TableCell>
+              <TableCell>{user.firstname}</TableCell>
+              <TableCell>{user.lastname}</TableCell>
+              <TableCell>{user.email}</TableCell>
+              <TableCell>{user.role}</TableCell>
+              <TableCell>
+                <span
+                  className={`inline-flex items-center rounded-full px-2.5 py-0.5 text-xs font-medium ${
+                    user.status
+                      ? "bg-green-100 text-green-800"
+                      : "bg-red-100 text-red-800"
+                  }`}
+                >
+                  {user.status ? "Active" : "Inactive"}
+                </span>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
 }
