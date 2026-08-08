2c0c0b7fd6f7cf814f595eff6c5e3d05c78f9e6f
Henry Edilio Ramirez Chevalier <henryedilio@gmail.com>
2026-08-08 01:19:21 -0400

    fix: update loading message to Spanish

--- app/(store)/(home)/loading.tsx
+++ app/(store)/(home)/loading.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Loading() {
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-white dark:bg-gray-900">
-      <p>Page is loading hang on a second</p>
+      <p>Pagina esta cargando</p>
     </div>
   );
 }
